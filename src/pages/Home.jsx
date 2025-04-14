89fd02ad742534c1def77313d33a23bfcdb4d554
Jacobo <juan233020@hotmail.com>
2025-04-14 13:29:02 -0500

    android commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,51 +86,51 @@ export default function Home() {
                 <div className="max-w-4xl mx-auto">
                     <h2 className="text-2xl sm:text-3xl font-bold mb-10 text-center">Contacto</h2>
                     <div className="space-y-6">
-                    {/* Email */}
-                    <div className="flex items-center bg-gray-900 p-6 rounded-lg hover:bg-gray-800 transition">
-                        <div className="w-16 h-16 flex items-center justify-center bg-blue-500/20 rounded-full mr-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-blue-400" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M24 4.5v15c0 .85-.65 1.5-1.5 1.5H21V7.387l-9 6.463-9-6.463V21H1.5C.649 21 0 20.35 0 19.5v-15c0-.425.162-.8.431-1.068C.7 3.16 1.076 3 1.5 3H2l10 7.25L22 3h.5c.425 0 .8.162 1.068.432.27.268.432.643.432 1.068z" />
-                        </svg>
+                        {/* Email */}
+                        <div className="flex items-center bg-gray-900 p-6 rounded-lg hover:bg-gray-800 transition">
+                            <div className="w-16 h-16 aspect-square flex items-center justify-center bg-blue-500/20 rounded-full mr-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-blue-400" fill="currentColor" viewBox="0 0 24 24">
+                                    <path d="M24 4.5v15c0 .85-.65 1.5-1.5 1.5H21V7.387l-9 6.463-9-6.463V21H1.5C.649 21 0 20.35 0 19.5v-15c0-.425.162-.8.431-1.068C.7 3.16 1.076 3 1.5 3H2l10 7.25L22 3h.5c.425 0 .8.162 1.068.432.27.268.432.643.432 1.068z" />
+                                </svg>
+                            </div>
+                            <div>
+                                <h3 className="text-lg font-semibold">Correo</h3>
+                                <a href="mailto:jacoboramirezcontacto@gmail.com" className="text-blue-400 hover:text-blue-300 transition block">
+                                    jacoboramirezcontacto@gmail.com
+                                </a>
+                                <p className="text-sm text-gray-400">Disponible para consultas, proyectos o colaboraciones.</p>
+                            </div>
                         </div>
-                        <div>
-                        <h3 className="text-lg font-semibold">Correo</h3>
-                        <a href="mailto:jacoboramirezcontacto@gmail.com" className="text-blue-400 hover:text-blue-300 transition block">
-                            jacoboramirezcontacto@gmail.com
-                        </a>
-                        <p className="text-sm text-gray-400">Disponible para consultas, proyectos o colaboraciones.</p>
-                        </div>
-                    </div>
 
-                    {/* WhatsApp */}
-                    <div className="flex items-center bg-gray-900 p-6 rounded-lg hover:bg-gray-800 transition">
-                        <div className="w-16 h-16 flex items-center justify-center bg-blue-500/20 rounded-full mr-6">
-                        <FontAwesomeIcon icon={faWhatsapp} className="text-blue-400 text-2xl" />
+                        {/* WhatsApp */}
+                        <div className="flex items-center bg-gray-900 p-6 rounded-lg hover:bg-gray-800 transition">
+                            <div className="w-16 h-16 aspect-square flex items-center justify-center bg-blue-500/20 rounded-full mr-6">
+                                <FontAwesomeIcon icon={faWhatsapp} className="text-blue-400 text-4xl" />
+                            </div>
+                            <div>
+                                <h3 className="text-lg font-semibold">WhatsApp</h3>
+                                <a href="https://wa.me/573170383544" className="text-blue-400 hover:text-blue-300 transition block">
+                                    +57 317 038 3544
+                                </a>
+                                <p className="text-sm text-gray-400">Contáctame directamente por mensaje.</p>
+                            </div>
                         </div>
-                        <div>
-                        <h3 className="text-lg font-semibold">WhatsApp</h3>
-                        <a href="https://wa.me/573170383544" className="text-blue-400 hover:text-blue-300 transition block">
-                            +57 317 038 3544
-                        </a>
-                        <p className="text-sm text-gray-400">Contáctame directamente por mensaje.</p>
-                        </div>
-                    </div>
 
-                    {/* LinkedIn */}
-                    <div className="flex items-center bg-gray-900 p-6 rounded-lg hover:bg-gray-800 transition">
-                        <div className="w-16 h-16 flex items-center justify-center bg-blue-500/20 rounded-full mr-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-blue-400" viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
-                        </svg>
+                        {/* LinkedIn */}
+                        <div className="flex items-center bg-gray-900 p-6 rounded-lg hover:bg-gray-800 transition">
+                            <div className="w-16 h-16 aspect-square flex items-center justify-center bg-blue-500/20 rounded-full mr-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-blue-400" viewBox="0 0 24 24" fill="currentColor">
+                                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                                </svg>
+                            </div>
+                            <div>
+                                <h3 className="text-lg font-semibold">LinkedIn</h3>
+                                <a href="https://linkedin.com/in/jacobo-ramírez-a9028935b" className="text-blue-400 hover:text-blue-300 transition block">
+                                    linkedin.com/in/jacobo-ramírez
+                                </a>
+                                <p className="text-sm text-gray-400">Conecta conmigo profesionalmente.</p>
+                            </div>
                         </div>
-                        <div>
-                        <h3 className="text-lg font-semibold">LinkedIn</h3>
-                        <a href="https://linkedin.com/in/jacobo-ramírez-a9028935b" className="text-blue-400 hover:text-blue-300 transition block">
-                            linkedin.com/in/jacobo-ramírez
-                        </a>
-                        <p className="text-sm text-gray-400">Conecta conmigo profesionalmente.</p>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </section>
